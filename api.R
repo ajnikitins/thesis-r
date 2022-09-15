7167723512083dbc01ad5e9e933359244f9e86ef
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2022-09-15 19:45:32 +0200

    initial commit

---
+++ api.R
@@ -0,0 +1,61 @@
+library(httr)
+library(jsonlite)
+
+## BITCOIN
+btc_res <- GET("https://blockchain.info/",
+               path = "address/357a3So9CbsNfBBgFYACGvxxS6tMaDoa1P",
+               query = list(limit = 5000, offset = 15000, format = "json"))
+
+btc_data <- fromJSON(rawToChar(btc_res$content))
+
+btc_data <- list()
+btc_offset <- 0
+repeat {
+  btc_res <- GET("https://blockchain.info/",
+                 path = "address/357a3So9CbsNfBBgFYACGvxxS6tMaDoa1P",
+                 query = list(limit = 5000, offset = btc_offset, format = "json"))
+
+  btc_data_part <- fromJSON(rawToChar(btc_res$content))
+
+  btc_data_part$txs <- rbind(btc_data$txs, btc_data_part$txs)
+  btc_data <- btc_data_part
+
+  btc_offset <- btc_offset + 5000
+  if (btc_offset >= btc_data_part$n_tx) break
+}
+
+
+
+## ETHER
+# eth_res <- GET("https://api.etherscan.io/api",
+#                query = list(module = "account",
+#                             action = "txlist",
+#                             address = "0x165CD37b4C644C2921454429E7F9358d18A45e14",
+#                             offset = 0,
+#                             page = 3,
+#                             sort = "desc",
+#                             apikey = "ZMEFGIGKHAQZ8W48EQED8WPCMH41QD579F"
+#                ))
+
+eth_res <- GET("https://api.covalenthq.com/",
+               path = "v1/1/address/0x165CD37b4C644C2921454429E7F9358d18A45e14/transactions_v2/",
+               query = list(key = "ckey_7c4c50530dd4418fbb30d995325", "no-logs" = "true", "page-size" = 99999)
+)
+
+# curl -X GET https://api.covalenthq.com/v1/1/address/0x165CD37b4C644C2921454429E7F9358d18A45e14/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=true&page-size=10&page-number=1&key=ckey_7c4c50530dd4418fbb30d995325
+# \ -H "Accept: application/json"
+
+eth_data <- fromJSON(rawToChar(eth_res$content))
+
+
+## USDT
+
+usdt_res <- GET("https://api.covalenthq.com/",
+               path = "v1/1/address/0x165CD37b4C644C2921454429E7F9358d18A45e14/transfers_v2/",
+               query = list(key = "ckey_7c4c50530dd4418fbb30d995325",
+                            # "no-logs" = "true",
+                            "page-size" = 99999,
+                            "contract-address" = "0xdac17f958d2ee523a2206206994597c13d831ec7")
+)
+
+usdt_data <- fromJSON(rawToChar(usdt_res$content))
