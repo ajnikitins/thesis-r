d68c6b56a568bb2bc887af92e0f257feef84715f
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2022-11-12 14:37:31 +0200

    create API for getting charity crypto addresses from Crystal

---
+++ src/api/crypto/addresses.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(rvest)
+
+get_crypto_addresses <- \() {
+  charities <- read_html("https://crystalblockchain.com/verified-ukraine-donation-programs-list/") %>%
+    html_elements(".un-block_content--organizations") %>%
+    map(html_elements, ".g-row") %>%
+    reduce(\(acc, charity) {
+      # Get name of charity
+      name <- charity %>% html_elements("h3") %>% html_text2()
+      # Get coins
+      coins <- charity[-1] %>% html_element(".g-cols--2") %>% html_text2()
+      # Get addresses
+      addresses <- charity[-1] %>% html_element(".g-cols--10") %>% html_text2()
+
+      if (length(addresses) > 0) {
+        out <- data.frame(name = name, coin = coins, address = addresses)
+        bind_rows(acc, out)
+      } else {
+        acc
+      }
+    }, .init = NULL)
+}
+
+crypto_addresses <- get_crypto_addresses()
+write.csv(crypto_addresses, "data/addresses.csv", row.names = FALSE)
