f9d9d5f35dcf50764e528c3bbf217b02608c974c
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2022-11-21 16:33:10 +0200

    Remove crypto donation outliers

--- src/api/crypto/crypto.R
+++ src/api/crypto/crypto.R
@@ -6,7 +6,8 @@ data_eth_txs <- readRDS("data/crypto/data_eth_txs.RDS")
 
 # TODO: Rename United24 and CBA addresses in .csv
 
-data_crypto <- bind_rows(data_btc_txs, data_eth_txs) %>%
+# Merge and aggregate
+data_crypto_merged <- bind_rows(data_btc_txs, data_eth_txs) %>%
   filter(name == "Official crypto wallets of Ukraine" | name == "The Return Alive Foundation") %>%
   mutate(name = case_when(
            name == "The Return Alive Foundation" ~ "CBA",
@@ -14,10 +15,17 @@ data_crypto <- bind_rows(data_btc_txs, data_eth_txs) %>%
          ),
          date = as_date(floor_date(time, "day")), .keep = "unused") %>%
   unite(type, name, type, sep = "_") %>%
+  # Remove outliers
+  # 2022-04-04 (2 >1 000 000 donations for U24 eth and btc wallets)
+  # 2022-07-08 (2 >250 000 donation for CBA btc wallet)
+  # 2022-07-30 (2 >250 000 donation for U24 btc wallet)
+  filter((date != "2022-04-04" & date != "2022-07-08" & date != "2022-07-30") | value_usd < 250000)
+
+data_crypto_agg <- data_crypto_merged %>%
   group_by(type, date) %>%
   summarise(count = n(), mean_usd = sum(value_usd) / count, .groups = "drop") %>%
   right_join(expand(., type, date = seq(dmy("01-01-2022"), max(date), by = 1)), by = c("type", "date")) %>%
   mutate(across(c(count, mean_usd), ~ replace_na(., 0))) %>%
   arrange(type, date)
 
-saveRDS(data_crypto, "data/crypto/data_crypto.RDS")
+saveRDS(data_crypto_agg, "data/crypto/data_crypto.RDS")
