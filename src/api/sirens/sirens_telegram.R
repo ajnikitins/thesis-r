e7df2811290ea5cd6d09a36127bce2f6cc613dc5
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2022-11-21 16:33:53 +0200

    Refactor siren data processing and add new variables

--- src/api/sirens/sirens_telegram.R
+++ src/api/sirens/sirens_telegram.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(lubridate)
+library(multidplyr)
 
 # data_sirens_raw_full <- read.csv("src/api/sirens/ukrainian-air-raid-sirens-dataset/datasets/full_data.csv", header = TRUE, encoding = "UTF-8")
 
@@ -16,22 +17,31 @@ data_sirens_oblast <- data_sirens_oblast_raw %>%
          interval = interval(floor_date(int_start(interval), "day"), floor_date(int_end(interval), "day")), .keep = "unused")
 
 # Given name of region and date, find sirens
-get_sirens <- \(data_sirens, date, name = ".all") {
+get_sirens <- \(date, name = ".all", data_sirens) {
   data_sirens %>%
-    filter((name == ".all" | enc2native(region) == enc2native(name)) & (date %within% interval))
+    filter((name == ".all" | region == name) & (date %within% interval))
 }
 
+cl_sirens <- new_cluster(parallel::detectCores())
+cluster_copy(cl_sirens, c("get_sirens", "data_sirens_oblast"))
+cluster_library(cl_sirens, c("dplyr", "purrr", "lubridate"))
+
 # Aggregate to date-siren count/duration
-data_sirens <- data.frame(date = as_date(seq(min(int_start(data_sirens_oblast$interval)), max(int_end(data_sirens_oblast$interval)), by = 60 * 60 * 24))) %>%
-  rowwise() %>%
-  mutate(sirens = list(get_sirens(data_sirens_oblast, date)),
-         sirens_kyiv = list(bind_rows(get_sirens(data_sirens_oblast, date, "Київ"), get_sirens(data_sirens_oblast, date, "Київська область"))),
-         siren_count_all = nrow(sirens),
-         siren_mean_duration_all = mean(sirens$duration_mins),
-         siren_count_kyiv = nrow(sirens_kyiv),
-         siren_mean_duration_kyiv = mean(sirens_kyiv$duration_mins)) %>%
-  ungroup() %>%
-  select(-sirens, -sirens_kyiv) %>%
+data_sirens <- expand(data_sirens_oblast,
+                      date = as_date(seq(min(int_start(data_sirens_oblast$interval)), max(int_end(data_sirens_oblast$interval)), by = 60 * 60 * 24)),
+                      region) %>%
+  group_by(date) %>%
+  partition(cl_sirens) %>%
+  mutate(sirens = map2(date, region, get_sirens, data_sirens_oblast),
+         siren_count = map_int(sirens, nrow),
+         siren_total_duration = map_dbl(sirens, ~ sum(.$duration_mins)),
+         siren_has = if_else(siren_count > 0, 1, 0)) %>%
+  collect() %>%
+  summarise(siren_count = sum(siren_count),
+            siren_mean_duration = sum(siren_total_duration) / sum(siren_count),
+            siren_mean_duration = if_else(is.nan(siren_mean_duration), 0, siren_mean_duration),
+            siren_prop = sum(siren_has) / n(),
+            siren_kyiv = if_else(any(siren_has == 1 & (region == "Київ" | region == "Київська область")), 1, 0)) %>%
   right_join(expand(., date = seq(dmy("01-01-2022"), max(date), by = 1)), by = "date") %>%
   mutate(across(-date, ~ replace_na(., 0))) %>%
   arrange(date)
