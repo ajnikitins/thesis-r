fa3de9905ad2cf2fd41f1f87e584fe45be1b4b2c
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2022-12-05 20:23:00 +0100

    Fixup deseasoning

--- src/deseasoning.R
+++ src/deseasoning.R
@@ -5,12 +5,6 @@ data <- readRDS("data/data_complete.RDS")
 
 #using regression residuals to deseasonalise absolute values
 #create weekday dummies
-data_res <- data %>%
-  filter(date >= ymd("2022-03-10")) %>%
-  pivot_wider(names_from = type, values_from = c(don_count, don_mean_usd, d_don_count, d_don_mean_usd)) %>%
-  mutate(across(c(-date, -siren_kyiv), ~ deseasonalise(cur_data(), cur_column()))) %>%
-  pivot_longer(contains(c("don_count", "don_mean_usd")), names_to = c(".value", "type"), names_pattern = "(d?_?don_count|d?_?don_mean_usd)_([A-Z].*)")
-
 # Given a variable and a dataset, calculate the deseasonalised values
 deseasonalise <- \(.data, var_y) {
   # Get weekend dummies
@@ -22,15 +16,19 @@ deseasonalise <- \(.data, var_y) {
            thursday = ifelse(weekday == "Thursday",1,0),
            friday = ifelse(weekday == "Friday",1,0),
            saturday = ifelse(weekday == "Saturday",1,0),
-           sunday = ifelse(weekday == "Sunday",1,0),
-           # Different effects for pre-card donations
-           banking = ifelse(date < "2022-05-26" & (monday == 1 | saturday == 1 | sunday == 1), 1, 0))
+           sunday = ifelse(weekday == "Sunday",1,0))
 
-  formula <- as.formula(paste0(var_y, "~ monday + tuesday + wednesday + thursday + friday + saturday + banking + banking:(monday + saturday)"))
+  formula <- as.formula(paste0(var_y, "~ monday + tuesday + wednesday + thursday + friday + saturday"))
   model <- lm(formula, data = data)
 
   model$residuals
 }
+
+data_res <- data %>%
+  filter(date >= ymd("2022-03-10")) %>%
+  pivot_wider(names_from = type, values_from = c(don_count, don_mean_usd, d_don_count, d_don_mean_usd)) %>%
+  mutate(across(c(-date, -siren_kyiv), ~ deseasonalise(cur_data(), cur_column()))) %>%
+  pivot_longer(contains(c("don_count", "don_mean_usd")), names_to = c(".value", "type"), names_pattern = "(d?_?don_count|d?_?don_mean_usd)_([A-Z].*)")
 
 # ## Deseasonalised counts
 # # Ukrainian
