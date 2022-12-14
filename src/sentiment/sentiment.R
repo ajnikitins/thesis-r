12f403937c329016250bef8b381c82a9a64ba305
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2022-12-14 14:06:10 +0100

    finish process for tweet text sentiment analysis

--- src/sentiment/sentiment.R
+++ src/sentiment/sentiment.R
@@ -2,6 +2,86 @@ library(tidyverse)
 library(mongolite)
 library(stopwords)
 library(textstem)
+library(jsonlite)
+
+# Process raw text for sentiment analysis
+process_text <- \(text) {
+  text %>%
+    tolower() %>%
+    # remove mentions
+    str_replace_all("@.+?\\b", " ") %>%
+    # remove the hash tag sign (#) but not the actual tag as this may contain information
+    str_replace_all("#", "") %>%
+    # remove URLs
+    str_replace_all("https?.*?(?=( |$))", " ") %>%
+    # remove noise phrases
+    str_replace_all("(\\\n)|(&amp;)|(&gt;)|(&lt;)", " ") %>%
+    # replace incorrect apostrophes
+    str_replace_all("’", "'") %>%
+    # remove stopwords
+    str_replace_all(paste0("\\b", paste(stopwords("en", simplify = FALSE), collapse = "\\b|\\b"), "\\b"), " ") %>%
+    # remove all punctuations, including the question and exclamation marks
+    str_replace_all("[:punct:]|\\|", " ") %>%
+    # remove digits
+    str_replace_all("[\\d](st|nd|rd|th)*", " ") %>%
+    # remove non-ASCII characters (including emojis)
+    str_replace_all("[^[:ascii:]]", " ") %>%
+    # remove one length words
+    str_remove_all("\\b[:alpha:]\\b") %>%
+    # remove extra whitespace
+    str_remove_all("(^ +)|( +$)|( +(?= ))") %>%
+    # split tokens
+    str_split(fixed(" ")) %>%
+    # lemmatize tokens
+    lemmatize_words()
+}
+
+# Setup NRC emotion lexicon
+get_nrc <-  \() {
+  nrc <- read.table("data/sentiment/NRC-Emotion-Lexicon-Senselevel-v0.92.txt", sep = "\t", header = FALSE, col.names = c("lemma", "emotion", "val")) %>%
+    separate_rows(lemma, sep = "--|, ") %>%
+    group_by(lemma, emotion) %>%
+    summarize(val = if_else(any(val == 1), 1, 0), .groups = "drop") %>%
+    pivot_wider(names_from = emotion, values_from = val)
+}
+
+# Finds emotions for all tokens in a tweet from a given lexicon
+get_emotions <- \(tokens, lexicon = get_nrc()) {
+  lexicon %>%
+    filter(lemma %in% tokens) %>%
+    summarize(across(-lemma, sum)) %>%
+    as.list()
+}
+
+# Get "main" emotion of a tweet by largest number of words representing each emotion
+# Returns "mixed" if there are conflicts
+get_main_emotion <- \(emot) {
+  max_emot <- emot %>%
+    unlist() %>%
+    `[`(c("anger", "anticip", "disgust", "fear", "joy", "sadness", "surprise", "trust")) %>%
+    `[`(which(. == max(.)))
+
+  if (length(max_emot) > 1) {
+    "mixed"
+  } else {
+    names(max_emot)[1]
+  }
+}
+
+# Get "main" sentiment (positive/negative) of a tweet by largest number of words representing each sentiment
+# Returns "mixed" if there are conflicts
+get_main_sentiment <- \(emot) {
+  max_sent <- emot %>%
+    unlist() %>%
+    `[`(c("positive", "negative")) %>%
+    `[`(which(. == max(.)))
+
+  if (length(max_sent) > 1) {
+    "mixed"
+  } else {
+    names(max_sent)[1]
+  }
+}
 
 # Connect to tweet database & load tweets
 db <- mongo(collection = "tweets", db = "thesis-data", url = Sys.getenv("MONGO_URL"))
@@ -11,35 +91,28 @@ it_tweets <- db$iterate(query = '{"lang": "en"}', fields = '{"text": 1}')
 raw_tweets <- it_tweets$batch(100)
 
 tweets <- raw_tweets %>%
-  bind_rows()
+  bind_rows() %>%
+  rename(tweet_id = `_id`)
 
 ## Process tweet text
 tweets_proc <- tweets %>%
   # set all words to lowercase
-  mutate(text = tolower(text)) %>%
-  # remove mentions
-  mutate(text = str_replace_all(text, "@.+?\\b", " ")) %>%
-  # remove the hash tag sign (#) but not the actual tag as this may contain information
-  mutate(text = str_replace_all(text, "#", "")) %>%
-  # remove URLs
-  mutate(text = str_replace_all(text, "https?.*?(?=( |$))", " ")) %>%
-  # remove noise phrases
-  mutate(text = str_replace_all(text, "(\\\n)|(&amp;)|(&gt;)|(&lt;)", " ")) %>%
-  # replace incorrect apostrophes
-  mutate(text = str_replace_all(text, "’", "'")) %>%
-  # remove stopwords
-  mutate(text = str_replace_all(text, paste0("\\b", paste(stopwords("en", simplify = FALSE), collapse = "\\b|\\b"), "\\b"), " ")) %>%
-  # remove all punctuations, including the question and exclamation marks
-  mutate(text = str_replace_all(text, "[:punct:]|\\|", " ")) %>%
-  # remove digits
-  mutate(text = str_replace_all(text, "[\\d]", " ")) %>%
-  # remove non-ASCII characters (including emojis)
-  mutate(text = str_replace_all(text, "[^[:ascii:]]", " ")) %>%
-  # remove one length words
-  mutate(text = str_remove_all(text, "\\b[:alpha:]\\b")) %>%
-  # remove extra whitespace
-  mutate(text = str_remove_all(text, "(^ +)|( +$)|( +(?= ))")) %>%
-  # split tokens
-  mutate(text = str_split(text, fixed(" "))) %>%
-  # lemmatize tokens
-  mutate(text = lemmatize_words(text))
+  mutate(processed_text = process_text(text), .keep = "unused")
+
+# Load NRC list
+nrc <- get_nrc()
+
+# Get emotions from text
+tweets_emot <- tweets_proc %>%
+  mutate(emotions = map(processed_text, get_emotions, nrc),
+         main_emotion = map(emotions, get_main_emotion),
+         main_sentiment = map(emotions, get_main_sentiment),
+         .keep = "unused")
+
+# Add emotions to new collection
+db_emotions <- mongo(collection = "emotions", db = "thesis-data", url = Sys.getenv("MONGO_URL"))
+db_emotions$insert(tweets_emot, auto_unbox = TRUE)
+
+# Clean-up
+db$disconnect()
+db_emotions$disconnect()
