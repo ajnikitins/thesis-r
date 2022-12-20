b47dd0e88c982187e50b3533db1b536a3f701418
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2022-12-20 20:34:51 +0100

    fix sentiment data processing

--- src/sentiment/collect_sentiment.R
+++ src/sentiment/collect_sentiment.R
@@ -13,9 +13,10 @@ raw_emotions <- db$aggregate(pipeline = pipeline_emotions)
 emotions <- raw_emotions %>%
   unnest_wider(emotions) %>%
   mutate(date = as_date(date),
-         across(-date, replace_na, 0))
+         across(-date, replace_na, 0, .names = "emot_{.col}"),
+         .keep = "unused")
 
-# saveRDS(emotions, "data/sentiment/data_emotions.RDS")
+saveRDS(emotions, "data/sentiment/data_emotions.RDS")
 
 # Get daily sentiments
 pipeline_sentiment <- '[{"$lookup": {"from": "tweets", "localField": "tweet_id", "foreignField": "_id", "pipeline": [{"$project": {"_id": 0, "created_at": 1}}], "as": "result"}}, {"$unwind": {"path": "$result"}}, {"$project": {"date": {"$dateFromString": {"dateString": "$result.created_at"}}, "main_emotion": 1, "main_sentiment": 1}}, {"$group": {"_id": {"date": {"$dateTrunc": {"date": "$date", "unit": "day"}}, "sentiment": "$main_sentiment"}, "count": {"$count": {}}}}, {"$group": {"_id": "$_id.date", "sentiments": {"$push": {"k": "$_id.sentiment", "v": "$count"}}}}, {"$project": {"_id": 0, "date": "$_id", "sentiments": {"$arrayToObject": "$sentiments"}}}, {"$sort": {"date": 1}}]'
@@ -25,8 +26,9 @@ raw_sentiments <- db$aggregate(pipeline = pipeline_sentiment)
 sentiments <- raw_sentiments %>%
   unnest_wider(sentiments) %>%
   mutate(date = as_date(date),
-         across(-date, replace_na, 0))
+         across(-date, replace_na, 0, .names = "sent_{.col}"),
+         .keep = "unused")
 
-# saveRDS(sentiments, "data/sentiment/data_sentiments.RDS")
+saveRDS(sentiments, "data/sentiment/data_sentiments.RDS")
 
 db$disconnect()
