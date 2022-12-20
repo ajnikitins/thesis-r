7371bfa0e2fa756c4de3d4b2cb66f77e377c0fb0
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2022-12-20 20:35:41 +0100

    fix constructing the complete dataset

--- src/api/complete.R
+++ src/api/complete.R
@@ -2,26 +2,27 @@ library(tidyverse)
 library(lubridate)
 
 # Merge fiat and crypto donations
-data_cba <- readRDS("data/cba/data_cba.RDS")
+data_cba <- readRDS("data/cba/data_cba_rows_usd.RDS")
 data_crypto <- readRDS("data/crypto/data_crypto.RDS")
 
 data_donations <- bind_rows(data_cba, data_crypto) %>%
+  mutate(date = as_date(date)) %>%
+  # Setup type names
+  mutate(type = case_when(
+    str_detect(currency, "UAH") ~ "Ukrainian",
+    str_detect(currency, "Bitcoin|Ethereum") ~ "Crypto",
+    TRUE ~ "Foreign"
+  ), .keep = "unused") %>%
   # Filter to period
   filter(date %within% interval("2022-01-01", "2022-10-31")) %>%
-  # Assign groups - UA, Foreign, Crypto
-  mutate(type = case_when(
-    str_detect(type, "Foreign|EUR|GBP|PLN|USD|CHF|CZK|EUR|NOK|PLZ|SEK") ~ "Foreign",
-    str_detect(type, "Bitcoin|Ethereum") ~ "Crypto",
-    # str_detect(type, "Кредит Дніпро|Cash|Fondy|UAH|UKR|Universal") ~ "Ukrainian",
-    TRUE ~ "Ukrainian"
-  )) %>%
-  # # Aggregate counts, means
+  # Trim bottom and top 1% of donations
+  group_by(type) %>%
+  filter(between(value_usd, quantile(value_usd, 0.01), quantile(value_usd, 0.99))) %>%
+  # Calculate daily counts and means
   group_by(type, date) %>%
-  # summarize(mean_usd = (count * mean_usd) / sum(count), count = sum(count), .groups = "drop") %>%
-  summarize(don_mean_usd = sum(count * mean_usd) / sum(count),
-            don_mean_usd = if_else(is.nan(don_mean_usd), 0, don_mean_usd),
-            don_count = sum(count),
-            .groups = "drop") %>%
+  summarise(don_count = n(), don_mean = sum(value) / don_count, don_mean_usd = sum(value_usd) / don_count, .groups = "drop") %>%
+  right_join(expand(., type, date = full_seq(date, 1)), by = c("type", "date")) %>%
+  mutate(across(c(don_count, don_mean, don_mean_usd), ~ replace_na(., 0))) %>%
   arrange(type, date)
 
 saveRDS(data_donations, "data/data_donations.RDS")
@@ -31,16 +32,19 @@ data_sirens <- readRDS("data/sirens/data_sirens.RDS")
 data_tweet_count <- readRDS("data/tweets/count/data_tweet_count_day.RDS")
 data_factiva <- readRDS("data/factiva.RDS")
 data_severity <- readRDS("data/severity/data_severity.RDS")
+data_emotions <- readRDS("data/sentiment/data_emotions.RDS")
+data_sentiments <- readRDS("data/sentiment/data_sentiments.RDS")
 
 data_complete <- data_donations %>%
   left_join(data_sirens, by = "date") %>%
   left_join(data_tweet_count, by = "date") %>%
+  left_join(data_emotions, by = "date") %>%
+  left_join(data_sentiments, by = "date") %>%
   left_join(data_factiva, by = "date") %>%
   left_join(data_severity, by = "date")
 
 data_complete_d <- data_complete %>%
-  mutate(don_count = ifelse(don_count == 0, NA, don_count),
-         don_mean_usd = ifelse(don_mean_usd == 0, NA, don_mean_usd),
-         across(c(don_count, don_mean_usd, siren_count, siren_mean_duration, siren_prop, tweet_count, factiva_count, cas_civ, cas_rus_mil, confl_evs), ~ log(.) - log(lag(.)), .names = "d_{.col}"))
+  mutate(across(c(-date, -type, -siren_kyiv), ~ . - lag(.), .names = "d_{.col}")) %>%
+  mutate(across(c(-date, -type, -siren_kyiv, -starts_with("d_")), ~ log(.) - log(lag(.)), .names = "dlog_{.col}"))
 
 saveRDS(data_complete_d, "data/data_complete.RDS")
