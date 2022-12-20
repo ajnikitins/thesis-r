5ddb7b774931f8f075eddcf2e824ea5c7ed5c86f
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2022-12-20 22:44:48 +0100

    add events to main dataset

--- src/api/complete.R
+++ src/api/complete.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(lubridate)
+library(readxl)
 
 # Merge fiat and crypto donations
 data_cba <- readRDS("data/cba/data_cba_rows_usd.RDS")
@@ -34,8 +35,15 @@ data_factiva <- readRDS("data/factiva.RDS")
 data_severity <- readRDS("data/severity/data_severity.RDS")
 data_emotions <- readRDS("data/sentiment/data_emotions.RDS")
 data_sentiments <- readRDS("data/sentiment/data_sentiments.RDS")
+data_events_raw <- read_excel("data/important_events.xlsx")
+
+data_events <- data_events_raw %>%
+  mutate(event_positive = if_else(coloring == 0, 1, 0, missing = 0),
+         event_negative = if_else(coloring == 1, 1, 0, missing = 0)) %>%
+  select(date, event_positive, event_negative)
 
 data_complete <- data_donations %>%
+  left_join(data_events, by = "date") %>%
   left_join(data_sirens, by = "date") %>%
   left_join(data_tweet_count, by = "date") %>%
   left_join(data_emotions, by = "date") %>%
@@ -43,8 +51,9 @@ data_complete <- data_donations %>%
   left_join(data_factiva, by = "date") %>%
   left_join(data_severity, by = "date")
 
+
 data_complete_d <- data_complete %>%
-  mutate(across(c(-date, -type, -siren_kyiv), ~ . - lag(.), .names = "d_{.col}")) %>%
-  mutate(across(c(-date, -type, -siren_kyiv, -starts_with("d_")), ~ log(.) - log(lag(.)), .names = "dlog_{.col}"))
+  mutate(across(c(-date, -type, -starts_with("event"), -siren_kyiv), ~ . - lag(.), .names = "d_{.col}"),
+         across(c(-date, -type, -starts_with("event"), -siren_kyiv, -starts_with("d_")), ~ log(.) - log(lag(.)), .names = "dlog_{.col}"))
 
 saveRDS(data_complete_d, "data/data_complete.RDS")
