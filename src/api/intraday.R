e1e81f4649920526b7f0e5ecf3fd9a58b539fb8f
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2023-02-06 14:24:15 +0200

    Streamline intraday dataset's creation, add other variable forms

--- src/api/intraday.R
+++ src/api/intraday.R
@@ -1,25 +1,18 @@
 library(tidyverse)
 library(lubridate)
 
-# Load complete event dataset
-data_strikes_raw <- read.csv("data/intraday/events_latest.csv")
-
-# Filter to only airstrikes and do further pre-processing
-data_strikes <- data_strikes_raw %>%
-  filter(t_airstrike_b == 1 | t_artillery_b == 1) %>%
-  select(date, time) %>%
-  mutate(date = ymd_hm(paste(date, time)),
-         date = floor_date(date, unit = "hours")) %>%
-  group_by(date) %>%
-  summarise(strike_count = n(), .groups = "drop")
-
-# data_strikes %>%
-#   ggplot(aes(x = datetime, y = strike_count)) + geom_line()
-
 data_donations <- readRDS("data/data_donations_hourly.RDS")
+data_strikes <- readRDS("data/data_strikes_hourly.RDS")
+data_tweet_count <- readRDS("data/tweets/count/data_tweet_count_hour.RDS")
 
 data_intraday <- data_donations %>%
   left_join(data_strikes, by = "date") %>%
-  mutate(strike_count = replace_na(strike_count, 0))
+  left_join(data_tweet_count, by = "date") %>%
+  group_by(type) %>%
+  mutate(across(c(-date, -contains("dum")), ~log(.), .names = "log_{.col}"),
+         across(c(-date, -contains("dum"), -contains("log_")), ~log(1 + .), .names = "log1_{.col}")) %>%
+  filter(date >= ymd("2022-03-16")) %>%
+  mutate(across(c(-date, -contains("dum")), ~findInterval(.x, quantile(.x, seq(0, 1, 0.2)), rightmost.closed = TRUE), .names = "{col}_quint")) %>%
+  ungroup()
 
 saveRDS(data_intraday, "data/data_intraday.RDS")
