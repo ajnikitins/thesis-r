a4d12a237689c82264d3ac0b68533e994c33209f
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2023-02-06 14:14:10 +0200

    Move hourly strike dataset creation to strikes.R

--- src/api/strikes.R
+++ src/api/strikes.R
@@ -5,14 +5,26 @@ library(lubridate)
 data_strikes_raw <- read.csv("data/intraday/events_latest.csv")
 
 # Filter to only airstrikes and do further pre-processing
-data_strikes <- data_strikes_raw %>%
+data_strikes_hourly <- data_strikes_raw %>%
   filter(t_airstrike_b == 1 | t_artillery_b == 1) %>%
-  select(date, t_airstrike_b, t_artillery_b) %>%
-  mutate(date = ymd(date)) %>%
+  select(date, time, t_airstrike_b, t_artillery_b) %>%
+  mutate(date = ymd_hm(paste(date, time)),
+         date = floor_date(date, unit = "hours")) %>%
   group_by(date) %>%
   summarise(strike_air_count = sum(t_airstrike_b),
             strike_arty_count = sum(t_artillery_b),
             strike_air_arty_count = strike_air_count + strike_arty_count,
-            .groups = "drop")
+            .groups = "drop") %>%
+  right_join(tidyr::expand(., date = full_seq(date, 3600)), by = "date") %>%
+  mutate(across(-date, ~ replace_na(., 0))) %>%
+  arrange(date)
+
+saveRDS(data_strikes_hourly, "data/data_strikes_hourly.RDS")
+
+# Aggregate to daily airstrikes
+data_strikes <- data_strikes_hourly %>%
+  mutate(date = floor_date(date, unit = "days")) %>%
+  group_by(date) %>%
+  summarise(across(everything(), sum), .groups = "drop")
 
 saveRDS(data_strikes, "data/data_strikes.RDS")
