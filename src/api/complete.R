dab76a02f12deef35d131c5feeee3e6887c0b93a
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2023-02-09 10:11:16 +0200

    Create a complete dataset starting from 22 Feb

--- src/api/complete.R
+++ src/api/complete.R
@@ -11,14 +11,16 @@ data_factiva <- readRDS("data/factiva.RDS")
 data_severity <- readRDS("data/severity/data_severity.RDS")
 data_emotions <- readRDS("data/sentiment/data_emotions.RDS")
 data_sentiments <- readRDS("data/sentiment/data_sentiments.RDS")
-data_events_raw <- read_excel("data/important_events.xlsx")
+data_events_raw <- read_excel("data/important_events_hourly.xlsx")
 
 data_events <- data_events_raw %>%
+  mutate(datetime = with_tz(ymd_hms(datetime, tz = "Europe/Kiev"), tzone = "UTC"),
+         date = date(datetime)) %>%
   mutate(event_positive_dum = if_else(coloring == 0, 1, 0, missing = 0),
          event_negative_dum = if_else(coloring == 1, 1, 0, missing = 0)) %>%
   select(date, event_positive_dum, event_negative_dum)
 
-data_complete <- data_donations %>%
+data_complete_full <- data_donations %>%
   left_join(data_events, by = "date") %>%
   left_join(data_sirens, by = "date") %>%
   left_join(data_strikes, by = "date") %>%
@@ -27,11 +29,16 @@ data_complete <- data_donations %>%
   left_join(data_sentiments, by = "date") %>%
   left_join(data_factiva, by = "date") %>%
   left_join(data_severity, by = "date") %>%
+  mutate(across(c(-date, -type), ~ replace_na(., 0))) %>%
   group_by(type) %>%
   mutate(date = as_date(date)) %>%
   mutate(across(c(-date, -contains("dum")), ~log(.), .names = "log_{.col}"),
          across(c(-date, -contains("dum"), -contains("log_")), ~. - dplyr::lag(.), .names = "d_{.col}"),
-         across(c(-date, -contains("dum"), -starts_with(c("d_", "log_"))), ~log(.) - log(dplyr::lag(.)), .names = "dlog_{.col}")) %>%
+         across(c(-date, -contains("dum"), -starts_with(c("d_", "log_"))), ~log(.) - log(dplyr::lag(.)), .names = "dlog_{.col}"))
+
+saveRDS(data_complete_full, "data/data_complete_full.RDS")
+
+data_complete <- data_complete_full %>%
   filter(date >= ymd("2022-03-16")) %>%
   mutate(across(c(-date, -contains("dum")), ~findInterval(.x, quantile(.x, seq(0, 1, 0.2)), rightmost.closed = TRUE), .names = "{col}_quint")) %>%
   ungroup()
