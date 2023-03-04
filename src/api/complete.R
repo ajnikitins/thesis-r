37771f7150a2d20e033f71566798218a9c60ed28
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2023-03-04 09:33:00 +0200

    Deseasonalise the news variables when making the dataset

--- src/api/complete.R
+++ src/api/complete.R
@@ -2,6 +2,13 @@ library(tidyverse)
 library(lubridate)
 library(readxl)
 
+WEEKDAYS <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+deseasonalise_variable <- \(col, col_weekday) {
+  mod <- lm(col ~ factor(col_weekday))
+  mod$residuals
+}
+
 # Load variables
 data_donations <- readRDS("data/data_donations.RDS")
 data_sirens <- readRDS("data/sirens/data_sirens.RDS")
@@ -41,13 +48,17 @@ data_complete_full <- data_donations %>%
   group_by(type) %>%
   mutate(across(c(-date, -contains("dum")), ~log(.), .names = "log_{.col}"),
          across(c(-date, -contains("dum"), -contains("log_")), ~. - dplyr::lag(.), .names = "d_{.col}"),
-         across(c(-date, -contains("dum"), -starts_with(c("d_", "log_"))), ~log(.) - log(dplyr::lag(.)), .names = "dlog_{.col}"))
+         across(c(-date, -contains("dum"), -starts_with(c("d_", "log_"))), ~log(.) - log(dplyr::lag(.)), .names = "dlog_{.col}")) %>%
+  # Generate weekday dummies
+  mutate(weekday = factor(weekdays(date), levels = WEEKDAYS),
+         days_since = (as.numeric(date - 19047)))
 
 saveRDS(data_complete_full, "data/data_complete_full.RDS")
 
 data_complete <- data_complete_full %>%
   filter(date >= ymd("2022-03-16")) %>%
-  mutate(across(c(-date, -contains("dum")), ~findInterval(.x, quantile(.x, seq(0, 1, 0.2)), rightmost.closed = TRUE), .names = "{col}_quint")) %>%
+  mutate(across(c(dlog_news_factiva_count, dlog_news_euro_count), ~ deseasonalise_variable(., weekday), .names = "{.col}_des")) %>%
+  # mutate(across(c(-date, -contains("dum")), ~findInterval(.x, quantile(.x, seq(0, 1, 0.2)), rightmost.closed = TRUE), .names = "{col}_quint")) %>%
   ungroup()
 
 saveRDS(data_complete, "data/data_complete.RDS")
