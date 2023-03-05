52b5ad691107c6688705f3d493e304a762bb1196
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2023-03-05 10:50:21 +0200

    Optimize CBA data processing

--- src/api/cba/cba_rows/cba_rows.R
+++ src/api/cba/cba_rows/cba_rows.R
@@ -13,7 +13,7 @@ data_cba_counts <- fromJSON(file = "src/api/cba/cba_rows/cba_counts.json") %>%
   arrange(date)
 
 # Load rows into
-data_cba_rows_load <- list.files("data/cba_rows", full.names = TRUE, pattern = "*.json") %>%
+data_cba_rows_load <- list.files("data_manual/cba_rows", full.names = TRUE, pattern = "*.json") %>%
   future_map(\(file) {
     print(paste0("reading ", file))
     fromJSON(file = file)
@@ -28,17 +28,13 @@ data_cba_rows_raw <- data_cba_rows_load %>%
   bind_rows()
 
 data_cba_rows <- data_cba_rows_raw %>%
-  mutate(date = ymd_hms(date)) %>%
+  mutate(date = ymd_hms(date),
+         value = as.numeric(amount),
+         value_usd = convert_currencies(as.numeric(value), from = "UAH", to = "USD", date = floor_date(date, unit = "day"))) %>%
+  filter(value >= 0) %>%
+  select(-amount, -comment, -source) %>%
   arrange(date)
 
 saveRDS(data_cba_rows, "data/cba/data_cba_rows.RDS")
 
 # data_cba_rows <- readRDS("data/cba/data_cba_rows.RDS")
-
-data_cba_rows_usd <- data_cba_rows %>%
-  mutate(currency = if_else(currency == "PLZ", "PLN", currency),
-         value = as.numeric(amount),
-         value_usd = convert_currencies(as.numeric(amount), from = currency, to = "USD", date = floor_date(date, unit = "day"))) %>%
-  select(-amount, -comment, -source)
-
-saveRDS(data_cba_rows_usd, "data/cba/data_cba_rows_usd.RDS")
