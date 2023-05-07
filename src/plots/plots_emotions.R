b2ea6939d8c3688b53aff19d30b0cdd84bb1ead4
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2023-05-07 12:39:55 +0300

    Finalise emotion plot

--- src/plots/plots_emotions.R
+++ src/plots/plots_emotions.R
@@ -11,10 +11,8 @@ library(ggsci)
 library(ggrepel)
 library(ggpattern)
 
-data <- readRDS("data/data_complete_full.RDS")
-
 ## Emotional profile
-data_emot <- data %>%
+data_emot <- readRDS("data/data_complete_full.RDS") %>%
   ungroup() %>%
   filter(type == "Ukrainian") %>%
   select(date, tweet_count, intersect(starts_with("emot_count"), ends_with("ml"))) %>%
@@ -42,16 +40,9 @@ emot_plot <- data_emot %>%
   mutate(emotion = factor(emotion, levels = emot_order)) %>%
   ggplot(aes(fill = emotion, y = count, x = date, pattern_density = emotion, pattern_shape = emotion)) +
   geom_bar_pattern(position = "stack", stat = "identity", pattern = "pch", pattern_density = 0.3, pattern_spacing = 0.02, pattern_size = 0, pattern_fill = "white") +
-  # scale_pattern_density_manual(values = rep_len(c(0, 0.4, 0.45), length(emot_order))) +
   scale_pattern_density_manual(values = c(0.3, 0, 0.4, 0, 0.4, 0.3, 0)) +
-  # scale_pattern_shape_manual(values = rep_len(c(NA, 21, 24), length(emot_order))) +
   scale_pattern_shape_manual(values = c(21, NA, 24, NA, 24, 21, NA)) +
-  # scale_fill_grey() +
-  # scale_fill_brewer(palette = "Pastel1") +
   scale_fill_uchicago() +
-  # scale_pattern_fill_manual(values = pal_uchicago()(length(emot_order))) +
-  # scale_fill_jco() +
-  # viridis::scale_fill_viridis(option = "cividis", discrete = TRUE) +
   scale_x_date(date_labels = "%b/%y", date_breaks = "1 month", limits = as.Date(c("2022-02-01", "2023-03-01"))) +
   theme_classic() +
   ylab("Proportion of tweets by emotion") +
