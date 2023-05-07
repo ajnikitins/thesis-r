fbda2988515bdc05aa406ed4b5846424c3c6e9bb
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2023-05-07 12:56:56 +0300

    Optimise collection of raw CBA data, save it as RDS

--- src/api/cba/cba_rows/cba_rows.R
+++ src/api/cba/cba_rows/cba_rows.R
@@ -6,26 +6,15 @@ library(priceR)
 
 plan(multisession, workers = parallel::detectCores())
 
-data_cba_counts <- fromJSON(file = "src/api/cba/cba_rows/cba_counts.json") %>%
-  tibble(tmp = .) %>%
-  hoist(tmp, date = 1, count = 2) %>%
-  mutate(date = ymd(date)) %>%
-  arrange(date)
-
 # Load rows into
-data_cba_rows_load <- list.files("data_manual/cba_rows", full.names = TRUE, pattern = "*.json") %>%
-  future_map(\(file) {
+data_cba_rows_raw <- list.files("data_manual/cba_rows", full.names = TRUE, pattern = "*.json") %>%
+  future_map_dfr(\(file) {
     print(paste0("reading ", file))
-    fromJSON(file = file)
+    result <- fromJSON(file = file) %>%
+      map_dfr("rows")
   })
 
-data_cba_rows_raw <- data_cba_rows_load %>%
-  unlist(recursive = FALSE) %>%
-  tibble(rows = .) %>%
-  hoist(rows, rows = 2) %>%
-  pull(rows) %>%
-  unlist(recursive = FALSE) %>%
-  bind_rows()
+write_rds(data_cba_rows_raw, "data/cba/data_cba_rows_raw.RDS")
 
 data_cba_rows <- data_cba_rows_raw %>%
   mutate(date = ymd_hms(date),
@@ -35,6 +24,6 @@ data_cba_rows <- data_cba_rows_raw %>%
   select(-amount, -comment, -source) %>%
   arrange(date)
 
-saveRDS(data_cba_rows, "data/cba/data_cba_rows.RDS")
+write_rds(data_cba_rows, "data/cba/data_cba_rows.RDS")
 
-# data_cba_rows <- readRDS("data/cba/data_cba_rows.RDS")
+# data_cba_rows <- read_rds("data/cba/data_cba_rows.RDS")
